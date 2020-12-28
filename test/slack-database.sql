663aa5dd2bb561f10e4c6d63a33dfe4b27c58c4e
NidhalWers <nteyeb75@gmail.com>
2020-12-28 11:11:33 +0100

    maj table sql

--- test/slack-database.sql
+++ test/slack-database.sql
@@ -1,39 +1,41 @@
 CREATE TABLE users (
-    id INT,
     email VARCHAR(255) UNIQUE,
     password VARCHAR(255),
     username VARCHAR(255),
     creation_date DATE,
-    PRIMARY KEY(id)
+    PRIMARY KEY(email)
 );
 
 CREATE TABLE channels (
-    id INT,
     name VARCHAR(255) UNIQUE,
-    admin_id INT,
+    admin_email VARCHAR(255),
     creation_date DATE,
     PRIMARY KEY(id),
-    FOREIGN KEY (admin_id) REFERENCES users(id)
+    FOREIGN KEY (admin_email) REFERENCES users(email)
 );
 
 CREATE TABLE friends (
-     pers1_id INT,
-     pers2_id INT,
-     PRIMARY KEY(pers1_id, pers2_id),
-     FOREIGN KEY(pers1_id) REFERENCES users(id),
-     FOREIGN KEY(pers2_id) REFERENCES users(id)
+     usr1_email VARCHAR(255),
+     usr2_email VARCHAR(255),
+     PRIMARY KEY(usr1_email, usr2_email),
+     FOREIGN KEY(usr1_email) REFERENCES users(email),
+     FOREIGN KEY(usr2_email) REFERENCES users(email)
 );
 
-CREATE TABLE chats (
-     id INT,
-     PRIMARY KEY(id)
+CREATE TABLE posts (
+     id INT AUTO_INCREMENT,
+     channel_name VARCHAR(255),
+     usr_email VARCHAR(255),
+     PRIMARY KEY(channel_name,id),
+     FOREIGN KEY(channel_name) REFERENCES channels(name),
+     FOREIGN KEY(usr_email) REFERENCES users(email)
 );
 
 CREATE TABLE members (
-     chat_id INT,
-     user_id INT,
-     PRIMARY KEY(chat_id, user_id),
-     FOREIGN KEY(user_id) REFERENCES users(id),
-     FOREIGN KEY(chat_id) REFERENCES chats(id)
+     channel_name VARCHAR(255),
+     user_email VARCHAR(255),
+     PRIMARY KEY(channel_name, user_email),
+     FOREIGN KEY(user_email) REFERENCES users(email),
+     FOREIGN KEY(channel_name) REFERENCES channels(name)
 );
 
